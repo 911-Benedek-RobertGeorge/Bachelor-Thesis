ae0d4189f05fb8258f5441fc3a487beb0953e980
911-Benedek-RobertGeorge <benedek_robert2001@yahoo.com>
2023-04-18 12:37:59 +0300

    Bug fix

--- contracts/WorkShare.sol
+++ contracts/WorkShare.sol
@@ -33,9 +33,20 @@ contract WorkShare is Initializable, Ownable{
         _;
     }
 
+
+///TODO 
+    // - add a tax for each project, 3%  (add somebody who can withdraw it from the contract
+    // - add a new nft to the address somebody has , maybe a mapping address - > string , where the string is the nft metadata or the nft from pinata
+    // maybe add a recursive payment ( transferng tokens in ethereum) 
+
+    enum State{ OPEN, INPROGRESS, COMPLETED, FAILED}
+      //must delete a project after finalize / not commpleted, or to make a variable there
+    //finalize function works even if we dont have an developer 
+  
     struct Project{
         address manager; //TODO MAYBE CHANGE THIS TO PAYABLE
         string shortDescription;
+        State state; 
         string requirementsDocumentCID; // IPFS pinata CID
         uint64 reward; // the number of workShareTokens for completing the task
         uint64 penalty; // applied if after deadline
@@ -61,22 +72,27 @@ contract WorkShare is Initializable, Ownable{
     
     //should  the person to apply necceserily or the admin can name it even if not from applications ? 
     function acceptApplication(uint32 _nrOfProject, address _applicant) public onlyAdmin {
+        //require(projects[_nrOfProject].applications[_applicant], "The address has not applied for this project.");
         require(msg.sender == projects[_nrOfProject].manager,"You can only accept applicants for your own project.");
         projects[_nrOfProject].acceptedDeveloper = payable(_applicant);
+        projects[_nrOfProject].state = State.INPROGRESS;
     }
 
     //Allow developers to apply for a project
     function applyForProject(uint32 _projectNumber) public {
+        require(projects[_projectNumber].state == State.OPEN, "You can only apply to open projects.");
         string memory email = developers[msg.sender];
-        require(bytes(email).length == 0, "You have to be registered first");
+        require(bytes(email).length != 0, "You have to be registered first");
         uint32 nrOfApplicants = projects[_projectNumber].nrOfApplicants ++; 
         projects[_projectNumber].applications[nrOfApplicants] = msg.sender;  ///indexed from 0 
     }
 
     //When the project is approved by the manager, giving the reward to the developer
-    function finalizeProject(uint32 _projectNumber) public onlyAdmin{   
-        require(msg.sender == projects[_projectNumber].manager,"You can only finalize your own projects.");
+    function finalizeProject(uint32 _projectNumber) public onlyAdmin{
+        
         Project storage project = projects[_projectNumber]; // get a refference to the project
+        require(project.state == State.INPROGRESS, "The project must be in progress to be finalized.");
+        require(msg.sender == project.manager,"You can only finalize your own projects.");
         uint finalReward = project.reward;
         if(project.deadline < block.timestamp)
         {
@@ -85,15 +101,18 @@ contract WorkShare is Initializable, Ownable{
             require(workShareToken.transfer(msg.sender,project.penalty),"The transfer of penalty tokens failed."); 
         }
         require(workShareToken.transfer(project.acceptedDeveloper, finalReward),"The transfer of the reward failed");
-         
+        project.state = State.COMPLETED;
     }
 
     //When a project has not been completed, if the manager agrees that the developer has been implicated he will get 20% of reward, 
     // otherwise the developer does not get anything
-    function projectNotCompleted(uint32 _projectNumber, bool _effort) public onlyAdmin{   
-        require(msg.sender == projects[_projectNumber].manager,"You can only finalize your own projects.");
-        Project storage project = projects[_projectNumber]; // get a refference to the project
-        require(project.deadline > block.timestamp , "The deadline has not passed. Give it some time");
+    function projectNotCompleted(uint32 _projectNumber, bool _effort) public onlyAdmin{
+        Project storage project = projects[_projectNumber]; // get a refference to the project 
+        require(project.state == State.OPEN ||
+                project.state == State.INPROGRESS,
+                "This project is not in progres nor open");
+        require(msg.sender == project.manager,"You can only finalize your own projects.");
+        require(project.deadline < block.timestamp , "The deadline has not passed. Give it some time.");
         uint finalReward = 0;
         if(_effort == true)
         {
@@ -103,19 +122,20 @@ contract WorkShare is Initializable, Ownable{
         }
          // send back the tokens to the project manager
         require(workShareToken.transfer(msg.sender,project.reward - finalReward),"Transfering back the tokens of the not completed project failed.");
+        project.state = State.FAILED;
     }
 
-    function initialize(address payable _token) public initializer{
+    function initialize(address payable _token) public initializer onlyOwner{
         workShareToken = WorkShareToken(_token);
         admins[msg.sender] = true;
     }
     
     //Create a project 
     event CreateProjectEvent(uint32 nrOfProjects,string _requirementsDocumentCID,uint64 _reward, uint64 _penalty, uint64 _deadline);
-    function createProject(string memory _requirementsDocumentCID,uint64 _reward, uint64 _penalty, uint64 _deadline) public onlyAdmin
+    function createProject(string memory _shortDescription,string memory _requirementsDocumentCID,uint64 _reward, uint64 _penalty, uint64 _deadline) public onlyAdmin
     {
         //give allowance to the smart contract to transfer tokens to its address
-        require(workShareToken.approve(address(this), _reward),"Approving contract to spend the reward tokens has failed");
+        //require(workShareToken.approve(address(this), _reward),"Approving contract to spend the reward tokens has failed");
         
          ///transfer the tokens to the smart contract
         require(workShareToken.transferFrom(msg.sender, address(this), _reward),"The transfer of reward to the contract has failed");
@@ -123,7 +143,10 @@ contract WorkShare is Initializable, Ownable{
         Project storage newProject = projects[nrOfProjects];
         ++nrOfProjects; // ++i cost less gas than i++
 
+        
         newProject.manager = msg.sender;
+        newProject.shortDescription = _shortDescription;
+        newProject.state = State.OPEN;
         newProject.requirementsDocumentCID = _requirementsDocumentCID;
         newProject.reward = _reward;
         newProject.penalty = _penalty;
@@ -142,9 +165,41 @@ contract WorkShare is Initializable, Ownable{
         admins[_remove] = false;
     }
 
+
+    //The developers are going to register with the address and email.
+    function register(string memory _email) public { 
+        require(bytes(_email).length == 0, "You are already registered");
+        developers[msg.sender] = _email;
+        ++nrOfDevelopers;
+    }
+
     function changeManagerOfProject(uint32 _projectNumber, address _newManager) public onlyOwner {
-        require(admins[_newManager], "The address must be an admin first.");  
+        require(admins[_newManager], "The address must have an admin role first.");  
         projects[_projectNumber].manager = _newManager;
+    }
+
+    //GETTERS
+    // function getAllProjects() public view returns(Project[] memory){
+    //     Project[] memory projectList = new Project[](nrOfProjects); 
+    //     for(uint32 i = 0; i < nrOfProjects; ++i){
+    //         projectList[i] = projects[i];
+    //     }
+    //     return projectList;
+    // }
+
+    // function getOpenProjects() public view returns(Project[] memory){
+    //      Project[] memory projectList = new Project[](nrOfProjects); 
+    //     for(uint32 i = 0; i < nrOfProjects; ++i){
+    //         if(projects[i].state == State.OPEN)
+    //             {
+    //                 projectList[i] = projects[i];
+    //             }
+    //     }
+    //     return projectList;
+    // }
+
+    function getEmailOfDeveloper(address _address) public view onlyAdmin returns(string memory) {
+        return developers[_address];
     }
     
 } 
